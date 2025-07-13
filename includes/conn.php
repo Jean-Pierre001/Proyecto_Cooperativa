aed92978804662df6fa8a861351a9113398b7a7d
Jean-Pierre001 <Agustin500cm@gmail.com>
2025-07-13 18:36:38 -0300

    conexion echa bien ahora si

--- includes/conn.php
+++ includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-$host = 'CooperativaLtda.infinityfreeapp.com'; // Reemplazá con el hostname real que ves en el panel
+$host = 'sql200.infinityfree.com'; // Reemplazá con el hostname real que ves en el panel
 $db   = 'if0_39464143_cooperativa'; // Reemplazá con el nombre completo de tu BD
 $user = 'if0_39464143'; // Reemplazá con tu usuario de la base de datos
 $pass = 'g8VCJB4WEf'; // Escribí tu contraseña real
